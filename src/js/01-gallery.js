2548d4bd607be9d81bbf9052eea584143280cb56
IrynaDiachenko <diachenko.iryna7@gmail.com>
2022-05-06 19:07:08 +0300

    gallery first

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,26 @@
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryEl = document.querySelector('.gallery');
+
+const galleryItemsString = galleryItems.map(item => (
+  `<a class="gallery__item" href="${item.original}">
+    <img
+      class="gallery__image"
+      src="${item.preview}"
+      alt="${item.description}"
+    />
+  </a>`
+).join('');
+galleryEl.insertAdjacentElement('afterbegin', galleryItemsString);
+
+let gallery = new SimpleLightbox('.gallery a', {
+    captionsData: 'alt',
+    captionDelay: 250,
+    captionPosition: 'botton',
+});
+
+// console.log(galleryItems);
