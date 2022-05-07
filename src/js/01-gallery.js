e549c20255cb41f49bde02b7a987b304a53bbc0e
IrynaDiachenko <diachenko.iryna7@gmail.com>
2022-05-07 22:16:04 +0300

    Correct task one

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,21 +1,19 @@
 import SimpleLightbox from 'simplelightbox';
-import 'simplelightbox/dist/simple-lightbox.min.css';
-// Add imports above this line
 import { galleryItems } from './gallery-items';
-// Change code below this line
+import 'simplelightbox/dist/simple-lightbox.min.css';
 
 const galleryEl = document.querySelector('.gallery');
 
-const galleryItemsString = galleryItems.map(item =>
-  `<a class="gallery__item" href="${item.original}">
-    <img
-      class="gallery__image"
-      src="${item.preview}"
-      alt="${item.description}"
-    />
-  </a>`.join(''),
-);
-galleryEl.insertAdjacentElement('afterbegin', galleryItemsString);
+const galleryItemsString = galleryItems
+  .map(
+    item =>
+      `<a class="gallery__item" href="${item.original}">
+  <img class="gallery__image" src="${item.preview}" alt="${item.description}" />
+</a>`,
+  )
+  .join('');
+
+galleryEl.insertAdjacentHTML('afterbegin', galleryItemsString);
 
 let gallery = new SimpleLightbox('.gallery a', {
   captionsData: 'alt',
