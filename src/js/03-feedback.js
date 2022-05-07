4857c2fd9a6ec91f368027031735e0c5a31086cf
IrynaDiachenko <diachenko.iryna7@gmail.com>
2022-05-07 23:10:39 +0300

    push second task 3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -12,3 +12,32 @@ const formData = {};
 
 refs.form.addEventListener('submit', onFormSubmit);
 refs.form.addEventListener('input', throttle(onFormInput, 500));
+
+populateTextarea();
+
+function onFormInput(event) {
+  formData[event.target.name] = event.target.value;
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+}
+
+function onFormSubmit(event) {
+  event.preventDefault();
+  event.target.reset();
+  console.log(
+    'feedback-form-state',
+    JSON.parse(localStorage.getItem(STORAGE_KEY)),
+  );
+  localStorage.removeItem(STORAGE_KEY);
+}
+
+function populateTextarea() {
+  const savedMessege = JSON.parse(localStorage.getItem(STORAGE_KEY));
+
+  if (savedMessege && savedMessege.message) {
+    refs.textarea.value = savedMessege.message;
+  }
+
+  if (savedMessege && savedMessege.email) {
+    refs.mail.value = savedMessege.email;
+  }
+}
