171f8146e37ba8a2e40c49d09543b4184c90b1fc
IrynaDiachenko <diachenko.iryna7@gmail.com>
2022-05-07 22:59:27 +0300

    First push task 3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,14 @@
+import throttle from 'lodash.throttle';
 
+const STORAGE_KEY = 'feedback-form-state';
+
+const refs = {
+  form: document.querySelector('.feedback-form'),
+  mail: document.querySelector('.feedback-form input'),
+  textarea: document.querySelector('feedback-form textarea'),
+};
+
+const formData = {};
+
+refs.form.addEventListener('submit', onFormSubmit);
+refs.form.addEventListener('input', throttle(onFormInput, 500));
